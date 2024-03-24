6d61b3163109810b6a97dcffcdb8f1e11ee6626f
Dephrish <ngocdunghk15@gmail.com>
2024-03-24 15:33:12 +0700

    Update UI

--- src/popup/components/app/CookiesETLForm.tsx
+++ src/popup/components/app/CookiesETLForm.tsx
@@ -1,17 +1,20 @@
-import {Alert, Button, Card, Form, Input, message, Upload} from "antd";
+import {Alert, Button, Card, Divider, Form, Input, message, Upload} from "antd";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCookie, faEllipsis, faFileExport, faFileImport} from "@fortawesome/free-solid-svg-icons";
+import {faCookie, faCopy, faEllipsis, faFileExport, faFileImport} from "@fortawesome/free-solid-svg-icons";
 import {useActiveTab} from "~hooks/tab.hook";
 import {getCookies, setCookies} from "~utils/cookies";
 import {useState} from "react";
 import {decryptPayload, encryptPayload} from "~utils/secure";
 import {downloadFile, getTime, onReadFileContent, validateFileExtension} from "~utils/app";
-import {FileFilterMode} from "~enum/app.enum";
+import {FileFilterMode, Status} from "~enum/app.enum";
 import type {RcFile} from "antd/lib/upload";
+import * as randomstring from 'randomstring';
 
 function CookiesETLForm() {
   const {origin, hostname} = useActiveTab();
   const [password, setPassword] = useState<string>();
+  const [token, setToken] = useState<string>('');
+  const [importStatus, setImportStatus] = useState<Status>(Status.IDLE);
 
   const uploadProps = {
     name: 'file',
@@ -21,18 +24,24 @@ function CookiesETLForm() {
     onChange: async (info) => {
       try {
         if (info?.file?.status === 'done') {
+          setImportStatus(Status.PENDING);
           if (!password) {
             message.error('Password is required for secure!');
             return;
           }
           const {file} = info;
           const encryptedFileContent = await onReadFileContent(file?.originFileObj);
+          console.log({password})
           const decryptedFileContent = await decryptPayload(encryptedFileContent, password);
           setCookies(decryptedFileContent?.cookies, decryptedFileContent?.url);
+          setImportStatus(Status.FULFILLED);
           message.success('Import cookies successfully!');
-          chrome.tabs.reload(function(){});
+          chrome.tabs.reload(function () {
+          });
         }
       } catch (error) {
+
+        setImportStatus(Status.REJECTED);
         message.error('Somethings went wrongs!')
       }
     },
@@ -53,19 +62,20 @@ function CookiesETLForm() {
 
   async function onExportCookies() {
     try {
-      if (!password) {
-        message.error('Password is required for secure!');
-        return;
-      }
       const cookies = await getCookies({url: origin});
       const payload = {
         url: origin,
         domain: hostname,
         cookies
       };
-      const encryptedPayload = encryptPayload(payload, password);
+      const token = randomstring.generate({
+        readable: false
+      });
+      setToken(token);
+      const encryptedPayload = encryptPayload(payload, token);
       downloadFile(encryptedPayload, `${hostname}_${getTime()}_encrypted.json`);
     } catch {
+      setToken('');
       message.error('Failed to encrypted!')
     }
   }
@@ -79,20 +89,36 @@ function CookiesETLForm() {
     <Form
       layout={'vertical'}
     >
-      <Form.Item className={'mb-4'} label={"Encryption password for the cookies data:"}>
-        <Input.Password
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder={"Password..."}/>
+      <Form.Item className={'mb-0'}>
+        <Button
+          block
+          onClick={onExportCookies}
+          type={'primary'}
+          icon={<FontAwesomeIcon icon={faFileExport}/>}>
+          Export
+        </Button>
       </Form.Item>
-      <div className={'flex gap-4 mb-4'}>
-        <Form.Item className={'mb-0'}>
-          <Button
-            onClick={onExportCookies}
-            type={'primary'}
-            icon={<FontAwesomeIcon icon={faFileExport}/>}>
-            Export
-          </Button>
+      {token &&
+          <Alert
+              className={'mt-3'}
+              message={`Secure information: ${token}`}
+              type="warning"
+              action={<Button type={'text'} shape={'circle'} onClick={() => {
+                navigator.clipboard.writeText(token)
+                  .then(() => {
+                    message.success('Copied to clipboard!')
+                  })
+              }}>
+                <FontAwesomeIcon icon={faCopy} color={'#dd9955'}/>
+              </Button>}
+              showIcon/>}
+      <Divider className={'my-4'}/>
+      <div className={'flex gap-4'}>
+        <Form.Item className={'mb-4 flex-grow-1'}>
+          <Input.Password
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder={"Password..."}/>
         </Form.Item>
         <Form.Item className={'mb-0'}>
           <Upload {...uploadProps}>
@@ -105,10 +131,15 @@ function CookiesETLForm() {
           </Upload>
         </Form.Item>
       </div>
-      <Alert
-        message="Import cookies successfullyyyyyyyy =))"
-        type="warning"
-        showIcon/>
+      {importStatus === Status.FULFILLED && <Alert
+          message="Import cookies successfullyyyyyyyy =))"
+          type="warning"
+          showIcon/>}
+      {importStatus === Status.REJECTED &&
+          <Alert
+              message="Faileddddddddd to import cookies =(("
+              type="error"
+              showIcon/>}
     </Form>
   </Card>;
 }
